88e9a471c95d2df5f65879faf2a624eab8249b9b
mingshi1214 <mingshichi@gmail.com>
2024-11-21 15:30:03 -0500

    fixed wandering robot

--- src/cpmr_ch2/cpmr_ch2/swarm.py
+++ src/cpmr_ch2/cpmr_ch2/swarm.py
@@ -5,10 +5,10 @@ from rclpy.node import Node
 from rclpy.parameter import Parameter
 from rcl_interfaces.msg import SetParametersResult
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Twist, Pose, Point, Quaternion
+from geometry_msgs.msg import Twist
 from std_msgs.msg import String
 from nav_msgs.msg import Odometry
-from gazebo_msgs.srv import GetEntityState
+from gazebo_msgs.msg import ModelStates
 from enum import Enum
 import time
 
@@ -63,9 +63,9 @@ def distance(x0, y0, x1, y1):
 
 class SwarmRobot(Node):
 
-    Rel_Pose_Pushing_Threshold = 0.04 #m
-    Object_Trans_Goal_Threshold = 0.2 #m
-    Object_Rot_Goal_Threshold = 0.1 #rads
+    Rel_Pose_Pushing_Threshold = 0.07 #m
+    Object_Trans_Goal_Threshold = 0.1 #m
+    Object_Rot_Goal_Threshold = 0.02 #rads
 
 
     def __init__(self):
@@ -95,15 +95,12 @@ class SwarmRobot(Node):
         self.declare_parameter('max_vel', value=self._max_vel)
         self.declare_parameter('vel_gain', value=self._vel_gain)
 
-        self._odom_sub = self.create_subscription(Odometry, "/odom", self._listener_callback, 5)
-        self._cmd_vel_pub = self.create_publisher(Twist, "/cmd_vel", 5)
-        self._diagnosis_pub = self.create_publisher(String, "/diagnosis", 5)
+        self._odom_sub = self.create_subscription(Odometry, "/odom", self._listener_callback, 1)
+        self._obj_sub = self.create_subscription(ModelStates, "/gazebo/model_states", self._box_callback, 1)
+        self._cmd_vel_pub = self.create_publisher(Twist, "/cmd_vel", 1)
+        self._diagnosis_pub = self.create_publisher(String, "/diagnosis", 1)
 
-        self._cli = self.create_client(GetEntityState, '/gazebo/get_entity_state')
-        while not self._cli.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('service not available, waiting again...')
-        self._req = GetEntityState.Request()
-        self._client_futures = []
+        self._cnt = 0
 
     def parameter_callback(self, params):
         self.get_logger().info(f'move_robot_to_goal parameter callback')
@@ -113,7 +110,7 @@ class SwarmRobot(Node):
             elif param.name == 'goal_y' and param.type_ == Parameter.Type.DOUBLE:
                 self._goal.y = param.value
             elif param.name == 'goal_t' and param.type_ == Parameter.Type.DOUBLE:
-                self._goal.t = param.value
+                self._goal.t = param.value + 0.1*param.value/(abs(param.value))
             elif param.name == 'max_vel' and param.type_ == Parameter.Type.DOUBLE:
                 self._max_vel = param.value
             elif param.name == 'vel_gain' and param.type_ == Parameter.Type.DOUBLE:
@@ -124,38 +121,26 @@ class SwarmRobot(Node):
             self.get_logger().warn(f"Changing goal {self._goal.x} {self._goal.y} {self._goal.t}")
         return SetParametersResult(successful=True)
 
+    def _box_callback(self, msg):
+        objs = msg.name
+        i = objs.index('unit_box')
+        box_pose = msg.pose[i]
+        self._box.x = box_pose.position.x
+        self._box.y = box_pose.position.y
+        o = box_pose.orientation
+        roll, pitch, self._box.t = euler_from_quaternion(o)
+
     def _listener_callback(self, msg):
         # listen to odom 
         # this is essentially the main loop
         # assigns odom and box pose and goes to state machine
         pose = msg.pose.pose
-        # self.get_logger().info(f'in listener')
 
         self._pose.x = pose.position.x
         self._pose.y = pose.position.y
         o = pose.orientation
         roll, pitch, self._pose.t = euler_from_quaternion(o)
 
-        # weird ros2 service client
-        self._req.name = 'unit_box'
-        self._client_futures.append(self._cli.call_async(self._req))
-        incomplete_futures = []
-        resp = None
-        for f in self._client_futures:
-            if f.done():
-                resp = f.result()
-            else:
-                incomplete_futures.append(f)
-        self._client_futures = incomplete_futures
-
-        if resp != None:
-            box_pose = resp.state.pose
-            self._box.x = box_pose.position.x
-            self._box.y = box_pose.position.y
-            o = box_pose.orientation
-            roll, pitch, self._box.t = euler_from_quaternion(o)
-
-        # self.get_logger().info(f'going to state machine')
         self._diagnosis()
         self._state_machine()
 
@@ -260,6 +245,7 @@ class SwarmRobot(Node):
     def _do_state_pushing_rot(self):
         if(abs(self._box.t - (self._box_init.t + self._goal.t)) <= SwarmRobot.Object_Rot_Goal_Threshold):
             self._cur_state = FSM_STATES.TASK_DONE
+            return
 
         # check if we're too far from our initial relative position
         curr_pose_rel_box = pose()
@@ -307,8 +293,6 @@ class SwarmRobot(Node):
     def _drive_to_goal(self, goal, max_pos_err=0.05):
         self._driving_goal.x = goal.x
         self._driving_goal.y = goal.y
-        self._driving_goal.t = goal.t
-        t_diff = goal.t - self._pose.t 
         
         x_diff = goal.x - self._pose.x
         y_diff = goal.y - self._pose.y
@@ -320,12 +304,20 @@ class SwarmRobot(Node):
             mag = np.sqrt((x_diff)**2 + (y_diff)**2)
             x = x_diff/mag * self._max_vel
             y = y_diff/mag * self._max_vel
+            
+            g0 = np.array([x, y])
+            # need to set in current robot frame (above is in robot init frame which is aligned with world frame in terms of rotation)
+            # robot rotates when getting dragged along block. do it in rotated robot frame
+            r_01 = np.array([[np.cos(self._pose.t), -np.sin(self._pose.t)],
+                            [np.sin(self._pose.t), np.cos(self._pose.t)]])
+            
+            g1 = r_01.T @ g0
 
             curr_mag = np.sqrt((x)**2 + (y)**2)
             # self.get_logger().info(f"vel mag {mag}, x {x}, y {y}, curr mag {curr_mag}")
 
-            twist.linear.x = x 
-            twist.linear.y = y 
+            twist.linear.x = g1[0] 
+            twist.linear.y = g1[1]
             
             # self.get_logger().info(f"at ({self._pose.x},{self._pose.y},{self._pose.t}) goal ({self._goal.x},{self._goal.y},{self._goal.t})")  
         # if abs(t_diff) > 0.1:
@@ -348,7 +340,7 @@ class SwarmRobot(Node):
         return
     
 def main(args=None):
-    
+
     rclpy.init(args=args)
     node = SwarmRobot()
     try:
